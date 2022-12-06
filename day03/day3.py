97ae935e6e43f30824cf17fb7139120a27a925d8
Adam Labadorf <alabadorf@gmail.com>
2022-12-05 21:27:09 -0500

    day 3 part 2

--- day03/day3.py
+++ day03/day3.py
@@ -2,8 +2,14 @@ import string
 letters = string.ascii_lowercase+string.ascii_uppercase
 priority_map = dict(zip(letters, range(1, len(letters)+1)))
 
+def get_badge(group):
+    in_common = set(group[0]).intersection(group[1]).intersection(group[2])
+    return list(in_common)[0]
+
 with open('input.txt') as f:
     tot_sum = 0
+    group_sum = 0
+    group = []
     for r in f:
         half = int(len(r)/2)
         r1, r2 = r[:half], r[half:]
@@ -12,4 +18,14 @@ with open('input.txt') as f:
 
         tot_sum += priority_map[in_common[0]]
 
-    print(tot_sum)
+        if len(group) == 3:
+            group_sum += priority_map[get_badge(group)]
+            group = [r.strip()]
+        else:
+            group.append(r.strip())
+
+    # last group badge
+    group_sum += priority_map[get_badge(group)]
+
+    print('Total priority: {}'.format(tot_sum))
+    print('Group priority: {}'.format(group_sum))
